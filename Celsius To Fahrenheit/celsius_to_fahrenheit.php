70dd9de9d3abb67ad96af689293184cce84f201c
Md Aktarul Islam <95102479+AKTARUL112@users.noreply.github.com>
2024-07-08 22:15:02 +0600

    Create celsius_to_fahrenheit.php

---
+++ Celsius To Fahrenheit/celsius_to_fahrenheit.php
@@ -0,0 +1,12 @@
+<?php
+
+// Temperature in Celsius
+$celsius = 32;
+
+// Formula for conversion (F = (C * 9/5) + 32)
+$fahrenheit = ($celsius * 9/5) + 32;
+
+// Print result with 2 decimal places using printf
+printf("Temperature in Fahrenheit: %.2f", $fahrenheit);
+
+?>
